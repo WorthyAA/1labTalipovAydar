1c5f57515caf5aa04119018464d26c98e2f06d8c
WorthyAA <adr254@internet.ru>
2025-09-16 16:42:11 +0500

    первый коммит(первый принт)

--- 1taskTalipovAydar.py
+++ 1taskTalipovAydar.py
@@ -2,5 +2,6 @@ def main():
  print('Талипов Айдар')
  print('ИИМРТ, ИВТ-ПО-102Б')
  print('16.09.2025')
+ print('Now I don't wanna feel a thing anymore') #коммитгитхаба
 if __name__ == '__main__':
     main()
